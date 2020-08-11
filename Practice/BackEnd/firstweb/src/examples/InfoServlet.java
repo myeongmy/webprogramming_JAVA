9e83eb3abcc6e290d6599736c296b84c7f84b96c
myeongmy <kp6069@naver.com>
2020-08-11 18:10:55 +0900

    Java Servlet Practice

---
+++ Practice/BackEnd/firstweb/src/examples/InfoServlet.java
@@ -0,0 +1,60 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InfoServlet
+ */
+@WebServlet("/info")
+public class InfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InfoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		String uri = request.getRequestURI();
+		StringBuffer url = request.getRequestURL();
+		String path = request.getContextPath();
+		String host = request.getRemoteHost();
+		
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+		
+		out.println("uri : "+uri+"<br>");
+		out.println("url : "+url+"<br>");
+		out.println("path : "+path+"<br>");
+		out.println("host : "+host+"<br>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
